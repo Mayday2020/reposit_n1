a1697a3368094acc0a1d133d6679a46b125f0bd0
Dmitry Likholetov <likholetov1990@gmail.com>
2020-01-25 22:10:16 +0300

    Урок 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,36 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+
 
 alert('... пешеходы по лужам, а вода по асфальту рекой.');
 console.log('Пусть бегут неуклюже...');
+
+let money = 30000;
+
+
+let income = 'Премия';
+
+
+let addExpenses = 'Подарки, Интернет, Угощения';
+
+
+let deposit = true;
+
+
+let mission = 300000;
+
+let period = 6;
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log('addExpenses: ', addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + 'рублей');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
