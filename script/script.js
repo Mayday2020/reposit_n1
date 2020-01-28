3539bf75bf719815ec734fb0677921323a832c4c
Dmitry Likholetov <likholetov1990@gmail.com>
2020-01-29 00:47:05 +0300

    Урок 4

--- script/script.js
+++ script/script.js
@@ -3,58 +3,80 @@
 alert('... пешеходы по лужам, а вода по асфальту рекой.');
 console.log('Пусть бегут неуклюже...');
 
-let money = 30000;
-let income = 'Премия';
-let addExpenses = 'Подарки, Интернет, Угощения';
-let deposit = true;
-let mission = 300000;
-let period = 6;
+let money = 30000,
+    income = 'Фриланс',
+    addExpenses = ' ',
+    deposit = true,
+    mission = 300000,
+    period = 6;
 
+money = prompt('Ваш месячный доход?', 30000);
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
-console.log('addExpenses: ', addExpenses.length);
-
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
-
-
-console.log(addExpenses.split(', '));
-
-
-
-money = prompt('Ваш месячный доход?');
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Бензин, связь, квартплата');
+console.log(addExpenses.split(', '));     //+
+console.log('addExpenses: ', addExpenses.length);    //+
 deposit = confirm('Есть ли у вас депозит в банке?');
 
-let expenses1 = prompt('Введите обязательную статью расходов');
+        // Выводит в консоль тип переменных
 
-let amount1 = prompt('Во сколько обойдется?');
+const showTypeOf = function(data) {
+    console.log(data, typeof(data));
+}
 
-let expenses2 = prompt('Введите обязательную статью расходов');
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
-let amount2 = prompt('Во сколько обойдется?');
+let expenses1 = prompt('Введите обязательную статью расходов', 'Бензин'),
+    amount1 = +prompt('Во сколько обойдется?', 2500),
+    expenses2 = prompt('Введите обязательную статью расходов', 'Связь'),
+    amount2 = +prompt('Во сколько обойдется?', 1300);
 
+        // Возвращает сумму всех обязательных расходов за месяц
 
-budgetMonth = money - (Number(amount1) + Number(amount2));
-console.log('Буджет на месяц: ' + budgetMonth);
+const getExpensesMonth = function(a, b) {
+    let sum = a + b;
+    return sum;
+};
+console.log(getExpensesMonth(amount1, amount2));
 
-console.log('Цель будет достигнута за: ' + Math.ceil(mission / budgetMonth) + ' месяцев.');
+        // Возврат накоплений за месяц (доход - расход)
 
-let budgetDay = budgetMonth / 30; // Заработок в сутки с учетом расходов
+const getAccumulatedMonth = function(c, d) {
+    const sun = c - d;
+    return sun;
+}
+let accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amount1, amount2));
 
+        //  Срок достижение цели
+
+const getTargetMonth = function(e, f){
+    return e / f;
+}
+getTargetMonth(mission, accumulatedMonth);
+console.log('Цель будет достигнута за: ' + Math.ceil(mission / accumulatedMonth) + ' месяцев.');
+
+        // Заработок в сутки с учетом расходов
+
+let budgetDay = accumulatedMonth / 30; 
 console.log('Бюджет на день: ' + Math.round(budgetDay));
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-}   else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log('У вас средний уровень дохода');
-}   else if (budgetDay < 600) {
-        if (budgetDay <= 0) {
-            console.log('Что то пошло не так');
-        } else {console.log('К сожалению у вас уровень дохода ниже среднего');}
-        
-}   
+
+        //Сообщение об уровне заработка
+
+let getStatusIncome = function(){
+    if (budgetDay >= 1200) {
+        return ('У вас высокий уровень дохода');
+    }   else if (budgetDay >= 600 && budgetDay < 1200) {
+        return ('У вас средний уровень дохода');
+    }   else if (budgetDay < 600) {
+            if (budgetDay <= 0) {
+                return ('Что то пошло не так');
+            } else {return ('К сожалению у вас уровень дохода ниже среднего');
+        }
+            
+    } 
+}
+console.log(getStatusIncome());
+
+  
+
